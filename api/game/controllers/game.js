9d1d0fda60351e8b5292a39ef6312178474f3762
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-09-13 01:40:18 +0100

    feat: Implements games services

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,17 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  populate: async (ctx) => {
+    console.log('Starting to populate...')
 
-module.exports = {};
+    const options = {
+      sort: "popularity",
+      page: 1,
+      ...ctx.query
+    }
+
+    await strapi.services.game.populate(options)
+
+    ctx.send(console.log('Finished populating'))
+  }
+};
